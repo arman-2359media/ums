89b9126a714b3703621b9dc7a937950f783c0fe7
arman-2359media <arman.rozika@2359media.com>
2019-06-13 10:14:12 +0700

    init app

---
+++ src/component/App.js
@@ -0,0 +1,18 @@
+import React, {Fragment} from 'react'
+
+function App(){
+
+
+    return (
+        <Fragment>
+            <div className="topbar">
+                User Management System
+            </div>
+            <div className="main-container">
+            
+            </div>
+        </Fragment>
+    )
+}
+
+export default App;
